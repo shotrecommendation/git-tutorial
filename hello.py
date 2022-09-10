10148e4b59d77d4b1d19ba11c21343d4525ee39d
shot <wojciech.wolujewicz@protonmail.com>
2022-09-10 11:40:28 +0200

    Added an author/email coment

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import sys
 
 # Default value is "World"
+# Author: Wojciech Wołujewicz (wojciech.wolujewicz@notmyrealemail.com)
 name = sys.argv[1] if len(sys.argv) > 1 else "World"
 
 print(f"Hello, {name}!")
